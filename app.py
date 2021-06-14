760894ca1d86850a3432aebbf863ce90e8efe666
SouravPal95 <sourav@filed.com>
2021-06-14 20:26:07 +0530

    Fixed oauth

--- app.py
+++ app.py
@@ -4,54 +4,47 @@ from googleapiclient.discovery import build
 
 from flask import Flask, redirect, url_for, session, jsonify, request
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app=Flask(__name__)
-cors=CORS(app)
-api=Api(app)
 
 app.config['SECRET_KEY']="23e01632e0d475932a8ecc3b1177bc574f9e0adbb9efd19f76f76b4faa860f20"
+app.config['SQLALCHEMY_DATABASE_URI']=r"sqlite:///D:\Abstract Inc (Filed)\google-api\data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
-class Authorize(Resource):
-    def get(self):
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'client_secrets_desk.json',
-            scopes=['https://www.googleapis.com/auth/content']
-        )
-        credentials=flow.run_local_server(host='localhost',
-                        port=8080,
-                        authorization_prompt_message='Please visit this URL: {url}', 
-                        success_message='The auth flow is complete; you may close this window.',
-                        open_browser=True)
-        credentials={
-            'token': credentials.token,
-            'refresh_token': credentials.refresh_token,
-            'token_uri': credentials.token_uri,
-            'client_id': credentials.client_id,
-            'client_secret': credentials.client_secret,
-            'scopes': credentials.scopes
-            }
-        
-        return credentials
+cors=CORS(app)
+api=Api(app)
+db=SQLAlchemy(app)
 
+class Credential(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    merchant_id=db.Column(db.Integer, nullable=False)
+    token=db.Column(db.String)
+    refresh_token=db.Column(db.String)
+    
+    def __repr__(self):
+        return f"Credential<merchant_id:{self.merchant_id}>"
 
 class ListProducts(Resource):
     def get(self, merch_id=433480089):
-
-        #print(f"from ListPRoducts: {session}")
-        headers = request.headers
-        if not headers.get("Refresh_Token"):
+            
+        merchant=Credential.query.filter_by(merchant_id=merch_id).first()
+        if not merchant:
             return {
                 'message': 'Unauthorized',
             }
+        
         credentials = {
-            'token': headers.get('Token', None),
-            'refresh_token': headers.get('Refresh_Token', None),
-            'token_uri': headers.get('Token_Uri', None),
-            'client_id': headers.get('Client_Id', None),
-            'client_secret': headers.get('Client_Secret', None),
-            'scopes': headers.get('Scopes', None).split(" ")
-        }
+            'token': merchant.token,
+            'refresh_token': merchant.refresh_token,
+            'token_uri': "https://oauth2.googleapis.com/token",
+            'client_id': "1092016066629-r8maka40hpbpelillfnoiq8r997ot2o7.apps.googleusercontent.com",
+            'client_secret': "nAadNvVsVH-NvwFMguWbzz-I",
+            'scopes': [
+                "https://www.googleapis.com/auth/content"
+            ]
+        } 
            
         credentials=Credentials(**credentials)
         with build('content', 'v2.1',
@@ -64,19 +57,23 @@ class ListProducts(Resource):
 
 class Product(Resource):
     def get(self, merch_id):
-        headers = request.headers
-        if not headers.get("Refresh_Token"):
+        
+        merchant=Credential.query.filter_by(merchant_id=merch_id).first()
+        if not merchant:
             return {
                 'message': 'Unauthorized',
             }
+        
         credentials = {
-            'token': headers.get('Token', None),
-            'refresh_token': headers.get('Refresh_Token', None),
-            'token_uri': headers.get('Token_Uri', None),
-            'client_id': headers.get('Client_Id', None),
-            'client_secret': headers.get('Client_Secret', None),
-            'scopes': headers.get('Scopes', None).split(" ")
-        }
+            'token': merchant.token,
+            'refresh_token': merchant.refresh_token,
+            'token_uri': "https://oauth2.googleapis.com/token",
+            'client_id': "1092016066629-r8maka40hpbpelillfnoiq8r997ot2o7.apps.googleusercontent.com",
+            'client_secret': "nAadNvVsVH-NvwFMguWbzz-I",
+            'scopes': [
+                "https://www.googleapis.com/auth/content"
+            ]
+        } 
            
         credentials=Credentials(**credentials)
         with build('content', 'v2.1', credentials=credentials) as service:
@@ -88,19 +85,22 @@ class Product(Resource):
         return jsonify(response)    
     
     def post(self, merch_id):
-        headers=request.headers
-        if not headers.get('Refresh_Token'):
+        merchant=Credential.query.filter_by(merchant_id=merch_id).first()
+        if not merchant:
             return {
-                "message": "Unauthorized"
+                'message': 'Unauthorized',
             }
+        
         credentials = {
-            'token': headers.get('Token', None),
-            'refresh_token': headers.get('Refresh_Token', None),
-            'token_uri': headers.get('Token_Uri', None),
-            'client_id': headers.get('Client_Id', None),
-            'client_secret': headers.get('Client_Secret', None),
-            'scopes': headers.get('Scopes', None).split(" ")
-        }
+            'token': merchant.token,
+            'refresh_token': merchant.refresh_token,
+            'token_uri': "https://oauth2.googleapis.com/token",
+            'client_id': "1092016066629-r8maka40hpbpelillfnoiq8r997ot2o7.apps.googleusercontent.com",
+            'client_secret': "nAadNvVsVH-NvwFMguWbzz-I",
+            'scopes': [
+                "https://www.googleapis.com/auth/content"
+            ]
+        } 
         credentials=Credentials(**credentials)
         product=request.get_json()
         with build('content', 'v2.1', credentials=credentials) as service:
@@ -111,19 +111,22 @@ class Product(Resource):
         return jsonify(response)
     
     def patch(self, merch_id):
-        headers=request.headers
-        if not headers.get('Refresh_Token'):
+        merchant=Credential.query.filter_by(merchant_id=merch_id).first()
+        if not merchant:
             return {
-                "message": "Unauthorized"
+                'message': 'Unauthorized',
             }
+        
         credentials = {
-            'token': headers.get('Token', None),
-            'refresh_token': headers.get('Refresh_Token', None),
-            'token_uri': headers.get('Token_Uri', None),
-            'client_id': headers.get('Client_Id', None),
-            'client_secret': headers.get('Client_Secret', None),
-            'scopes': headers.get('Scopes', None).split(" ")
-        }
+            'token': merchant.token,
+            'refresh_token': merchant.refresh_token,
+            'token_uri': "https://oauth2.googleapis.com/token",
+            'client_id': "1092016066629-r8maka40hpbpelillfnoiq8r997ot2o7.apps.googleusercontent.com",
+            'client_secret': "nAadNvVsVH-NvwFMguWbzz-I",
+            'scopes': [
+                "https://www.googleapis.com/auth/content"
+            ]
+        } 
         credentials=Credentials(**credentials)
         product=request.get_json()
         with build('content', 'v2.1', credentials=credentials) as service:
@@ -135,19 +138,22 @@ class Product(Resource):
         return jsonify(response)   
     
     def delete(self, merch_id):
-        headers = request.headers
-        if not headers.get("Refresh_Token"):
+        merchant=Credential.query.filter_by(merchant_id=merch_id).first()
+        if not merchant:
             return {
                 'message': 'Unauthorized',
             }
+        
         credentials = {
-            'token': headers.get('Token', None),
-            'refresh_token': headers.get('Refresh_Token', None),
-            'token_uri': headers.get('Token_Uri', None),
-            'client_id': headers.get('Client_Id', None),
-            'client_secret': headers.get('Client_Secret', None),
-            'scopes': headers.get('Scopes', None).split(" ")
-        }
+            'token': merchant.token,
+            'refresh_token': merchant.refresh_token,
+            'token_uri': "https://oauth2.googleapis.com/token",
+            'client_id': "1092016066629-r8maka40hpbpelillfnoiq8r997ot2o7.apps.googleusercontent.com",
+            'client_secret': "nAadNvVsVH-NvwFMguWbzz-I",
+            'scopes': [
+                "https://www.googleapis.com/auth/content"
+            ]
+        } 
            
         credentials=Credentials(**credentials)
         with build('content', 'v2.1', credentials=credentials) as service:
@@ -158,57 +164,53 @@ class Product(Resource):
         
         return jsonify(response) 
     
-@app.route('/authorize2')
-def authorize2():
-    
-    flow = Flow.from_client_secrets_file(
-        'client_secrets_web.json',
-        scopes=['https://www.googleapis.com/auth/content'],
-        redirect_uri=url_for('oauth2callback', 
-                             _external=True)
-    )
-    authorization_url, state=flow.authorization_url(
-        access_type="offline",
-        include_granted_scopes='true'
-    )
-    #print(f"state from authorize2: {state}")
-    session['state']=state
-    
-    return {"Authorization Url": authorization_url}
-    #return redirect(authorization_url)
+class Authorize(Resource):
+    def get(self, merchant_id):
+        
+        flow = Flow.from_client_secrets_file(
+            'client_secrets_web.json',
+            scopes=['https://www.googleapis.com/auth/content'],
+            redirect_uri=url_for('oauth2callback', 
+                                _external=True),
+            state=merchant_id
+        )
+        authorization_url, state=flow.authorization_url(
+            access_type="offline",
+            include_granted_scopes='true'
+        )
+        print(f"state from authorize2: {state}")
+        session['state']=state
+        
+        merchant=Credential(merchant_id=merchant_id)
+        db.session.add(merchant)
+        db.session.commit() 
+        return {
+            "Authorization Url": authorization_url
+        }
 
 @app.route('/oauth2callback')
 def oauth2callback():
     
-    print(f"state from oauth2callback: {session.get('state')}")
     flow=Flow.from_client_secrets_file(
         'client_secrets_web.json',
         scopes=['https://www.googleapis.com/auth/content'],
-        state=session.get('state'),
         redirect_uri=url_for('oauth2callback', _external=True)
     )
     authorization_resp=request.url
     flow.fetch_token(authorization_response=authorization_resp)
-    credentials = {
-        'token': flow.credentials.token,
-        'refresh_token': flow.credentials.refresh_token,
-        'token_uri': flow.credentials.token_uri,
-        'client_id': flow.credentials.client_id,
-        'client_secret': flow.credentials.client_secret,
-        'scopes': flow.credentials.scopes
-    }
-    session['credentials']=credentials
-    #print(f"From Oauth2callback: {session}")
-    #return redirect(url_for('index'))
-    #return redirect(url_for('products', merch_id=session['merch_id']))
-    #return redirect(api.url_for(Products, merch_id=session['merch_id']))
-    #return redirect(session.get('original_url') or url_for('index'))
     
-    return credentials
+    merchant=Credential.query.filter_by(merchant_id=request.args.get('state')).first()
+    merchant.token=flow.credentials.token
+    merchant.refresh_token=flow.credentials.refresh_token
+    db.session.commit()
+ 
+    return {
+        "Message":f"credentials added to db {request.args.get('state')}"
+    }
 
 api.add_resource(ListProducts, '/shop/listproducts', '/shop/<int:merch_id>/listproducts')
 api.add_resource(Product, '/shop/<int:merch_id>/product')
-api.add_resource(Authorize, '/authorize')
+api.add_resource(Authorize, '/<int:merchant_id>/authorize2')
 
 if __name__=="__main__":
     app.run()
